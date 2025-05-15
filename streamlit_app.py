2adb1630f492b7b065dd70c2b24604f088af7304
sruju2002 <srujwalajh@gmail.com>
2025-05-15 17:59:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.session import session
+from snowflake.snowpark.session import Session
 
 session = session.builder.configs(st.secrets["connections"]["snowflake"]).create()
 
